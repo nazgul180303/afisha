dc0551bf21cfa37fc06f2c1cd77535840b224de4
nazgul180303 <nazgul180303@gmail.com>
2023-12-25 17:01:04 +0600

    hw1

---
+++ moovie_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MoovieAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'moovie_app'
---
+++ moovie_app/views.py
@@ -0,0 +1,49 @@
+
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Director, Movie, Review
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+
+
+@api_view(['GET'])
+def director_list(request):
+    directors = Director.objects.all()
+    serializer = DirectorSerializer(directors, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def director_detail(request, id):
+    director = get_object_or_404(Director, id=id)
+    serializer = DirectorSerializer(director)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def movie_list(request):
+    movies = Movie.objects.all()
+    serializer = MovieSerializer(movies, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def movie_detail(request, id):
+    movie = get_object_or_404(Movie, id=id)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def review_list(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def review_detail(request, id):
+    review = get_object_or_404(Review, id=id)
+    serializer = ReviewSerializer(review)
+    return Response(serializer.data)
